c42a96ceba797ece70b1c832ef6f51a761867e18
MrHakimoto <fodp1116@seuemail.com>
2021-10-08 17:09:06 -0300

    first commit

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+
+<body>
+
+    <form action="resposta.php">
+        <div>
+            <p>
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome">
+            </p>
+            <p>
+                <label for="email">Email:</label>
+                <input type="email"  id="email">
+            </p>
+            <p>
+                <label for="cpf">Digite seu CPF:</label>
+                <input type="number" max="11">
+            </p>
+            <p>
+                <label for="senha1">Digite uma senha:</label>
+                <input type="text" id="senha1">
+            </p>
+            <p>
+                <label for="senha2">Repita a Senha:</label>
+                <input type="text" id="senha2">
+            </p>
+        </div>
+
+
+
+
+    </form>
+</body>
+
+</html>
